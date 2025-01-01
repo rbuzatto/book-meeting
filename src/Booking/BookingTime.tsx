d75a108d026b282b0d4ad740a90ee5fa4335e424
rbuzatto <rafael.buzatto@energage.com>
2025-01-01 16:49:27 -0300

    create booking time component

---
+++ src/Booking/BookingTime.tsx
@@ -0,0 +1,111 @@
+import { TimePicker } from '@/components/TimePicker'
+import { useGetSlotTimesForMonth } from '@/useGetSlotTimesForMonth'
+import clsx from 'clsx'
+import { useState } from 'react'
+import { format, parseISO, isBefore } from 'date-fns'
+
+function groupByDay(dates: string[]) {
+  return dates.reduce(
+    (acc, date) => {
+      const dayKey = format(new Date(date), 'yyyy-MM-dd')
+      if (!(dayKey in acc)) {
+        acc[dayKey] = []
+      }
+      acc[dayKey].push(date)
+      return acc
+    },
+    {} as Record<string, string[]>,
+  )
+}
+
+export const BookingTime = () => {
+  const [pickedMonthDate, setPickedMonthDate] = useState<Date>(new Date())
+  const { data, isLoading, error, isError } = useGetSlotTimesForMonth(pickedMonthDate)
+  const [selectedDay, setSelectedDay] = useState<string>('')
+  const [selectedtSlot, setSelectedSlot] = useState<string | null>(null)
+
+  console.log('isError:', isError, error)
+
+  console.log(data)
+  if (isLoading) return null
+  const slots = data?.data?.available_times ?? []
+  const slotsByDay = groupByDay(slots)
+  const enabledDays = new Set(Object.keys(slotsByDay))
+  const isDisabledDay = (date: Date) => {
+    return isBefore(date, new Date()) || !enabledDays.has(format(date, 'yyyy-MM-dd'))
+  }
+  const hasPickedDay = true
+  const confirmSlot = () => {
+    console.log('Time confirmed:')
+  }
+
+  const selectedSlotsForDay = selectedDay in slotsByDay ? slotsByDay[selectedDay] : []
+  return (
+    <div className="flex flex-col justify-center gap-4 p-7">
+      <h2 className="font-bold text-xl">Select a Date & Time</h2>
+      <div className="flex items-center justify-center w-full">
+        <TimePicker
+          onMonthChange={setPickedMonthDate}
+          month={pickedMonthDate}
+          disabled={isDisabledDay}
+          mode="single"
+          onSelect={slot => slot && setSelectedDay(format(new Date(slot), 'yyyy-MM-dd'))}
+          selected={parseISO(selectedDay)}
+        />
+        {hasPickedDay ? (
+          <div className="flex flex-col items-center justify-center w-full">
+            <ul className="flex flex-col gap-y-2 w-40">
+              {selectedSlotsForDay.map((slot, i) =>
+                selectedtSlot !== slot ? (
+                  <PickTime
+                    key={i}
+                    time={format(slot, 'HH:mm')}
+                    onClick={() => setSelectedSlot(slot)}
+                  />
+                ) : (
+                  <PickedTime key={i} time={format(slot, 'HH:mm')} onClick={confirmSlot} />
+                ),
+              )}
+            </ul>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  )
+}
+
+const baseClass =
+  'flex items-center justify-center w-full rounded-md p-2 text-md font-semibold border'
+
+type PickTimeProps = {
+  time: string
+  onClick: () => void
+}
+const PickTime = ({ time, onClick }: PickTimeProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={clsx(baseClass, ' text-blue-500 border-blue-200 hover:border-blue-500')}
+    >
+      {time}
+    </button>
+  )
+}
+
+type PickedTimeProps = {
+  time: string
+  onClick: () => void
+}
+const PickedTime = ({ time, onClick }: PickedTimeProps) => {
+  return (
+    <div className="flex justify-center w-full gap-2">
+      <div className={clsx(baseClass, 'text-white bg-gray-500')}>{time}</div>
+      <button
+        onClick={onClick}
+        className={clsx(baseClass, 'text-white bg-blue-500 hover:bg-blue-700')}
+      >
+        Next
+      </button>
+    </div>
+  )
+}
