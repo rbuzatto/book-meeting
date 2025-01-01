a996eb8ccab1c4694d69fef54a56e52f0038fbf3
rbuzatto <rafael.buzatto@energage.com>
2025-01-01 16:50:15 -0300

    create useGetSlotTimesForMonth hook

---
+++ src/useGetSlotTimesForMonth.ts
@@ -0,0 +1,25 @@
+import { startOfMonth, endOfMonth, format } from 'date-fns'
+import { useQuery } from '@tanstack/react-query'
+import { getData } from './api/api'
+import { BOOKING } from './api/endpoints'
+
+type ResponseData = {
+  data: {
+    available_times: string[]
+  }
+}
+
+export const useGetSlotTimesForMonth = (date: Date) => {
+  const startDate = startOfMonth(date)
+  const endDate = endOfMonth(date)
+
+  const startOftheMonth = format(startDate, "yyyy-MM-dd'T'HH:mm:ss")
+  const endOftheMonth = format(endDate, "yyyy-MM-dd'T'HH:mm:ss")
+  return useQuery<ResponseData>({
+    queryKey: ['available_times', startOftheMonth, endOftheMonth],
+    queryFn: async () =>
+      getData<ResponseData>(BOOKING.AVAILABLE_TIMES, [startOftheMonth, endOftheMonth], {
+        'x-mock-response-name': format(date, 'MMMyyyy'),
+      }),
+  })
+}
