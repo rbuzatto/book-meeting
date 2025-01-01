55df8477deaed0845e47c3c718300502e5b5bacd
rbuzatto <rafael.buzatto@energage.com>
2025-01-01 19:53:55 -0300

    add hover to day calendar button

--- src/components/TimePicker.tsx
+++ src/components/TimePicker.tsx
@@ -15,10 +15,10 @@ export const TimePicker = ({ isLoading, ...props }: TimePickerProps) => {
         disabled
         classNames={{
           day: ` w-13 h-13`,
-          day_button: `m-1 bg-blue-50 rounded-full w-11 h-11 text-blue-500 font-bold`,
+          day_button: `m-1 bg-blue-50 enabled:hover:bg-blue-100 rounded-full w-11 h-11 text-blue-500 font-bold`,
           selected: `[&_button]:bg-blue-500 [&_button]:text-white font-bold rounded-full border-0`,
           chevron: `${defaultClassNames.chevron} p-2 w-8 h-8`,
-          disabled: `[&_button]:bg-transparent [&_button]:text-gray-400 [&_button]:font-normal`,
+          disabled: `[&_button]:bg-transparent hover:bg-transparent [&_button]:text-gray-400 [&_button]:font-normal`,
           month: `${defaultClassNames.month} h-full flex flex-col items-center justify-center gap-4`,
           month_caption: `${defaultClassNames.month_caption} h-8 flex items-center justify-center`,
           weekday: `${defaultClassNames.weekday} font-normal text-xs`,
